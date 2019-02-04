fb00318677484008a55585a2ec495de55f47f2cf
Sarathsathasivam <47174083+Sarathsathasivam@users.noreply.github.com>
2019-02-04 13:31:04 +0530

    Update whether a number is positive or negative or zero.py

--- whether a number is positive or negative or zero.py
+++ whether a number is positive or negative or zero.py
@@ -1,9 +1,7 @@
-def check_number(n):
+n=int(raw_input())
   if n == 0:
     print ("Zero")
   elif n > 0:
     print ("Positive")
   else :
     print ("Negative")
-user_no = int(input(" "))
-check_number(user_no)
